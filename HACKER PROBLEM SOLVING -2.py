703ed2573f0ad61340ba0550f316b20fd23f0a10
Anbazhagan R <147583133+ANBU5994@users.noreply.github.com>
2025-02-01 17:18:08 +0530

    HACKER PROBLEM SOLVING -2.py

--- HACKER PROBLEM SOLVING -2.py
+++ HACKER PROBLEM SOLVING -2.py
@@ -1,5 +1,4 @@
 '''Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
-
 str.isalnum()
 This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
 
